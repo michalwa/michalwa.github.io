0e2a7e27c41b571f63c322d5114383ba1a428995
michalwa <michalwa2003@gmail.com>
2022-08-31 00:16:13 +0200

    Tweak some styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./_drafts/**/*.html",
+    "./_includes/**/*.html",
+    "./_layouts/**/*.html",
+    "./_posts/*.md",
+    "./*.md",
+    "./*.html",
+  ],
   theme: {
+    fontFamily: {
+      sans: ["IBM Plex Sans"],
+    },
     extend: {},
   },
-  plugins: [],
+  plugins: ["@tailwindcss/typography"],
 };
